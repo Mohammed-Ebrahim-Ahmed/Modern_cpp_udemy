a69df105ad4456c688123cfa0d7160571faaffcd
Mohammed-Ebrahim-Ahmed <mohammed.ebrahim.hassanin@gmail.com>
2024-08-09 16:46:20 +0300

    adding some features also

--- lab44/main.cpp
+++ lab44/main.cpp
@@ -13,6 +13,27 @@ public:
     }
 };
 
+class ExpressionOrder {
+    public:
+        ExpressionOrder& first(int){
+            std::cout << "first function is called"<< std::endl;
+            return *this;
+        }
+        ExpressionOrder& second(int){
+            std::cout << "second function is called"<< std::endl;
+            return *this;
+        }
+        int firstSubExrpession(int x)
+        {
+            std::cout << "firstSubExpression function is called" << std::endl;
+            return 0;
+        }
+        int secondSubExrpession(int x)
+        {
+            std::cout << "secondSubExpression function is called" << std::endl;
+            return 0;
+        }        
+};
 class foo
 {
 public:
@@ -22,31 +43,34 @@ public:
 
 int main()
 {
-    std::pair<int, int> p{2, 3};
-    auto [first, second] = p;
+    // std::pair<int, int> p{2, 3};
+    // auto [first, second] = p;
 
-    std::cout << first << " " << second << std::endl;
+    // std::cout << first << " " << second << std::endl;
 
-    std::map<std::string, int> mp{
-        {"what", 1},
-        {"why", 2},
-        {"how", 3}};
+    // std::map<std::string, int> mp{
+    //     {"what", 1},
+    //     {"why", 2},
+    //     {"how", 3}};
 
-    for (auto [key, val] : mp)
-    {
-        std::cout << key << " " << val << std::endl;
-    }
+    // for (auto [key, val] : mp)
+    // {
+    //     std::cout << key << " " << val << std::endl;
+    // }
 
-    int arr[3]{1, 2, 3};
-    auto prr = arr;
-    auto [f, s, t] = arr;
-    std::cout << f << " " << s << " " << t << std::endl;
-    dumdum d{29, 99};
-    auto &[money, age] = d;
-    money = 89;
-    age = 29;
-    std::cout << d.age << " " << d.money << std::endl;
+    // int arr[3]{1, 2, 3};
+    // auto prr = arr;
+    // auto [f, s, t] = arr;
+    // std::cout << f << " " << s << " " << t << std::endl;
+    // dumdum d{29, 99};
+    // auto &[money, age] = d;
+    // money = 89;
+    // age = 29;
+    // std::cout << d.age << " " << d.money << std::endl;
 
-    auto [crr, rrr] = foo();
+    // auto [crr, rrr] = foo();
+    std::cout << "****************************************" << std::endl;
+    ExpressionOrder eo;
+    eo.first(eo.firstSubExrpession(2)).second(eo.secondSubExrpession(3));
     return 0;
 }
